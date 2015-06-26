641f3e08989d957a338a7fded1f352436b2044dd
zsk526 <zsk526@qq.com>
2015-06-26 10:09:40 +0800

    init supplier

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+﻿
+jQuery(window).load(function() {
+
+   "use strict";
+
+   // Page Preloader
+   jQuery('#preloader').delay(350).fadeOut(function(){
+      jQuery('body').delay(350).css({'overflow':'visible'});
+   });
+});
