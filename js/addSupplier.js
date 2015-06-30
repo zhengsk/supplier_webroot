a3b8ed18ed71896d2a257846d9df7e2df01baa5d
zsk526 <zsk526@qq.com>
2015-06-30 16:54:23 +0800

    version 0.1

--- js/addSupplier.js
+++ js/addSupplier.js
@@ -1,7 +1,7 @@
-
+﻿
 jQuery(document).ready(function() {
 
-	var SupplierTypeSelect = new function(){
+	var SupplierTypeSelect = new (function(){
 
 		// 已经选中的供方类别
 		this.selectedTypes = [];
@@ -55,94 +55,159 @@ jQuery(document).ready(function() {
 
 		// 设置类别子项（右侧）
 		this.setDetailType = function (datas) {
+			var self = this;
 			$('#supplierTypeLabelTitle').html(datas.text); // 标题
 
-			var supplierTypeLabelWrapper = $('#supplierTypeLabelWrapper');
-			supplierTypeLabelWrapper.empty().hide(); // 先清空内容
+			var wrapper = $('#supplierTypeLabelWrapper');
+			wrapper.empty().hide(); // 先清空内容
 
 			if(datas.children){ // 存在子节点
 				var childNodes = datas.children;
 				if(childNodes[0].leaf){
-					createItem(childNodes);
+					wrapper.append(this._createItem(childNodes));
 				}else{
 					for(var m = 0, n = childNodes.length; m < n; m++){
 						var item = childNodes[m];
 						if(!item.leaf){
-							createItem(item.children, item.text);
+							wrapper.append(this._createItem(item.children, item.text));
 						}else{
-							createItem([item]);
+							wrapper.append(this._createItem([item]));
 						}
 					}
 				}
 			}else{
-				createItem([datas]);
+				wrapper.append(this._createItem([datas]));
 			}
 
-			supplierTypeLabelWrapper.fadeIn(400); //先清空内容
+			wrapper.fadeIn(400); //先清空内容
+		};
 
-			// 创建类型元素
-			function createItem(itemData, subtitle){
-				if(subtitle){
-					subtitle = $('<h5 class="subtitle strong">'+ subtitle +'</h5>');
-				}
-				var labels = $('<p></p>');
-				var ele;
-				for(var m = 0, n = itemData.length; m < n; m++){
-					$('<span class="label">'+ itemData[m].text +'</span>')
-						.data('typeDatas', itemData[m]).appendTo(labels);
-				}
-				var result = $('<div class="mb20"></div>').append(subtitle).append(labels);
-				supplierTypeLabelWrapper.append(result);
+		// 创建类型元素
+		this._createItem = function(itemData, subtitle){
+			var self = this;
+			if(subtitle){
+				subtitle = $('<h5 class="subtitle strong">'+ subtitle +'</h5>');
 			}
+			var labels = $('<p></p>');
+			var ele;
+			for(var m = 0, n = itemData.length; m < n; m++){
+				var label = $('<span class="label">'+ itemData[m].text +'</span>')
+					.data('typeDatas', itemData[m]);
+				itemData[m].node = label[0]; // typeDatas引用会标签 ， TODO: 是否会造成内存泄露
+				if(self.indexOfSelected(itemData[m]) !== -1){
+					label.addClass('label-checked');
+				}
+				label.appendTo(labels);
+			}
+			var result = $('<div class="mb20"></div>').append(subtitle).append(labels);
+			return result;
 
 			/*====
-				<div class="mb20">
-				    <h5 class="subtitle strong">土建工程施工总承包</h5>
-				    <p>
-				        <span class="label">住宅</span>
-				        <span class="label">公建</span>
-				        <span class="label">装配式住宅</span>
-				    </p>
-				</div>
-			====*/
-		};
+			<div class="mb20">
+			    <h5 class="subtitle strong">土建工程施工总承包</h5>
+			    <p>
+			        <span class="label">住宅</span>
+			        <span class="label">公建</span>
+			        <span class="label">装配式住宅</span>
+			    </p>
+			</div>
+		====*/
+		}
 
 		// 添加选中类别
 		this.addType = function(item){
 			this.selectedTypes.push(item);
+			this.showSelected(); // 显示已选中
 		};
 
 		// 删除已选类别
 		this.deleteType = function(item){
+			var index = this.indexOfSelected(item);
+			if(index !== -1){
+				this.selectedTypes.splice(index,1);
+			}
+			this.showSelected(); // 显示已选中
+		};
+
+		// 选中或删除类别
+		this.toggleSelect = function(item){
+			var index = this.indexOfSelected(item);
+			if(index === -1){
+				this.addType(item);
+				$(item.node).addClass('label-checked');
+			}else{
+				this.deleteType(item);
+				$(item.node).removeClass('label-checked');
+			}
+		}
+
+		// 返回是否选中，并返回索引值，否则返回-1
+		this.indexOfSelected = function(item){
 			var selectedTypes = this.selectedTypes;
 			for(var i = 0, j = selectedTypes.length; i < j; i++){
-				if(item.id === selectedTypes[i].id){
-					selectedTypes.splice(i,1);
-					return;
+				if(item === selectedTypes[i]){
+					return i;
 				}
 			}
-		};
+			return -1;
+		}
+
+		// 显示已选中的供方类别
+		this.showSelected = function(){
+			var tagsinput = $('#tags_tagsinput').empty();
+			var items = this.selectedTypes, tag;
+			if(items.length){
+				for(var i = 0, j = items.length; i < j; i++){
+					tag = $('<span class="tag"><span>'+ items[i].text +'</span> </span>');
+					$('<a href="javascript:;" title="删除">x</a>').data('typeDatas', items[i]).appendTo(tag);
+					tagsinput.append(tag);
+				}
+			}else{
+				tagsinput.html('没有选中的类别！');
+			}
+
+			/*=======
+				<span class="tag">
+				    <span>外墙装饰</span>
+				    <a href="#" title="删除">x</a>
+				</span>
+			=======*/
+		}
+
+		// 初始化
+		this.init = function(param){
+			var self = this;
+
+			// 加载供方类型数据
+			this.loadTypeData({
+				url : param.supplierTypeDataUrl
+			}, function(){
+				this.setAccordionType(); // 设置类别大类选择栏
+
+				//初始化，打开选中第一个
+				$('#accordionSupplierType').find('.collapsed').first().click();
+				$('#accordionSupplierType').find('.list-group-item').first().click();
+			});
+
+			// 绑定已选中类别删除事件
+			$('#tags_tagsinput').on('click', 'a', function(event){
+				self.toggleSelect($(this).data('typeDatas'));
+			});
+
+			// 绑定子类别选中和取消选中事件
+			$('#supplierTypeLabelWrapper').on('click', 'span.label', function(event){
+				self.toggleSelect($(this).data('typeDatas'));
+			});
+		}
 		 
-	}();
+	})();
 
 
 	/* ===================================== */
 
-
-	// 加载供方类型数据
-	SupplierTypeSelect.loadTypeData({
-		url : "_temp/supplierTypeData.json"
-	}, function(){
-		this.setAccordionType(); // 设置类别大类选择栏
+	SupplierTypeSelect.init({
+		supplierTypeDataUrl : "_temp/supplierTypeData.json"
 	});
-	
-
-	;(function() {
-		$('#supplierTypeLabelWrapper').on('click', 'span.label', function(event){
-			SupplierTypeSelect.deleteType(this.supplierType);
-		});
-	})();
-
 
 
 });
