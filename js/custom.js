04d62649a4851bd20cd42eca68ec71440309bf64
zsk526 <zsk526@qq.com>
2015-07-02 09:57:54 +0800

    edit

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,28 @@
-﻿
-jQuery(window).load(function() {
+﻿jQuery(window).load(function() {
 
-   "use strict";
+    "use strict";
 
-   // Page Preloader
-   jQuery('#preloader').delay(350).fadeOut(function(){
-      jQuery('body').delay(350).css({'overflow':'visible'});
-   });
+    // Page Preloader
+    jQuery('#preloader').delay(350).fadeOut(function() {
+        jQuery('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    });
+
+    // Tooltip
+    jQuery('.tooltips').tooltip({
+        container: 'body'
+    });
+
 });
-
-
 /*
  * 通用的Ajax调用方法
  */
 function doAjax (options, callBack) {
 
     var defaultOptions = {
-        type : "GET"
-        ,dataType : "json"
+        type: "GET"
+        ,dataType: "json"
         //,data: {}
     }
 
@@ -26,16 +31,16 @@ function doAjax (options, callBack) {
     options = $.extend(true, defaultOptions, options);
 
     $.ajax(options)
-	    .done(function(rep) {
-	        if(rep.status && rep.status === 500){
-	        	alert(rep.msg);
-	            return;
-	        }else{
-	        	callBack.call(options, rep);
-	        	return;
-	        }
-	    })
-	    .fail(function() {
-	        alert('加载出错！');
-	    });
+        .done(function(rep) {
+            if (rep.status && rep.status === 500) {
+                alert(rep.msg);
+                return;
+            } else {
+                callBack.call(options, rep);
+                return;
+            }
+        })
+        .fail(function() {
+            alert('加载出错！');
+        });
 }
