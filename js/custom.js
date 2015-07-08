51bf27967996576a3560a689eed17bc324e035b5
zsk526 <zsk526@qq.com>
2015-07-08 10:46:12 +0800

    edit 2015-07-07

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,57 @@ function doAjax (options, callBack) {
             alert('加载出错！');
         });
 }
+
+
+/*
+ * 表单加载数据和获取数据
+ */
+FormData = {
+    load : function load(formId, data){ // 参考easyui form部分
+        var form = $('#'+formId);
+        _load(data);
+        
+        function _load(data){
+            for(var name in data){
+                var val = data[name];
+                if (!_checkField(name, val)){
+                    if (!_loadBox(name, val)){
+                        form.find('input[name="'+name+'"]').val(val);
+                        form.find('textarea[name="'+name+'"]').val(val);
+                        form.find('select[name="'+name+'"]').val(val);
+                    }
+                }
+            }
+        }
+        
+        /**
+         * check the checkbox and radio fields
+         */
+        function _checkField(name, val){
+            var cc = $(target).find('input[name="'+name+'"][type=radio], input[name="'+name+'"][type=checkbox]');
+            if (cc.length){
+                cc.each(function(){
+                    _isChecked($(this).val(), val)
+                });
+                return true;
+            }
+            return false;
+        }
+        function _isChecked(v, val){
+            if (v == String(val) || $.inArray(v, $.isArray(val)?val:[val]) >= 0){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    get : function(formId, fields){
+        var form = $('#'+formId);
+        var fields = fields || form.find('input','textarea','select');
+        var result = {};
+        jQuery.each(fields, function(i, ele){
+            result[$(ele).attr('name')] = ele.value;
+        });
+        return result; 
+    }
+}
---
+++ js/supplier_edit_unbrand.js
@@ -0,0 +1,169 @@
+﻿
+jQuery(document).ready(function() {
+
+	var SupplierUnbrand = new (function(){
+
+		// 供方详细信息
+		this.supplierDatas = {
+			contacts : [{
+				area : '厦门',
+				person : '吴慧慧',
+				phone : '0592-1234567',
+				Email : 'wuhuihui@qq.com'
+			},{
+				area : '福州',
+				person : '吴是慧慧',
+				phone : '05952-1234567',
+				Email : 'wuhu订单ihui@qq.com'
+			}]
+		};
+
+		// 供方详细信息
+		this.labelDatas = {
+			contacts : [{
+				label : '地区',
+				field : 'area'
+			},{
+				label : '联系人',
+				field : 'person'
+			},{
+				label : '电话',
+				field : 'phone'
+			},{
+				label : 'Email',
+				field : 'Email'
+			}]
+		};
+
+		// 显示数据信息
+		this._panelShowDatas = function (labels, datas, panelId, callBack) {
+			var self = this;
+			var result = [], element;
+			$.each(datas, function(m, data){
+				for(var i = 0, j = labels.length; i < j; i++){
+					var label = labels[i];
+					element = self._createLabelElement(label.label, data[label.field]);
+					result.push(element);
+				}
+
+				var panelBody = $('<div class="panel-body"></div>');
+				var row = $('<div class="row form-horizontal"></div>');
+				row.html(result.join('')).appendTo(panelBody);
+				
+				result.length = 0;
+				data.element = panelBody; 			// data 引用到元素上
+				panelBody.data("rowDatas", data); 	// 绑定数据到元素上:rowDatas
+				$('#'+panelId).append(panelBody);
+			});
+
+			(callBack || jQuery.noop).call(this); //回调
+		};
+
+		// 创建一个名值元素
+		this._createLabelElement = function(label, value){
+
+			return '<div class = "col-sm-6"> <div class = "form-group"> <label class = "col-sm-4 control-label" >'+ label +'：</label> <div class="col-sm-8"> <input type="text" value="'+ value +'" class="form-control form-only-show" disabled /> </div> </div> </div>'
+			/*=========
+				<div class="col-sm-6">
+				    <div class="form-group">
+				        <label class="col-sm-4 control-label">
+				            地区：
+				        </label>
+				        <div class="col-sm-8">
+				            <input type="text" name="name" value="厦门" class="form-control form-only-show" disabled>
+				        </div>
+				    </div>
+				</div>
+			=========*/
+		}
+		
+
+
+		// 联系方式处理
+		this.contacts = {
+			supplier : this,
+
+			// 创建联系方式
+			show : function(){
+				var supplier = this.supplier;
+				supplier._panelShowDatas(
+					supplier.labelDatas.contacts, 
+					supplier.supplierDatas.contacts, 
+					"panelContact",
+					function(){
+						console.info(supplier);
+						console.info('added contact!');
+					}
+				);
+			}
+
+			// 添加联系方式
+			add : function(data){
+				var supplier = this.supplier;
+				supplier.contacts.push(data);
+			}
+
+			// 删除联系方式
+			remove : function(data){
+				var supplier = this.supplier;
+				jQuery.each(supplier.contacts, function(i,item){
+					if(item === data){
+						supplier.contacts.splice(i,1);
+						data.element.fadeOut();
+						data.element = null;
+						return;
+					}
+				});
+			}
+
+			update : function(data){
+
+			}
+		}
+
+		// 初始化
+		this.init = function(param){
+
+			this.contacts.show(); // 创建联系方式
+			
+
+		}
+		 
+	})();
+
+
+	/* ===================================== */
+
+	SupplierUnbrand.init({
+		supplierTypeDataUrl : "_temp/supplierTypeData.json"
+	});
+
+	
+});
+
+
+// 联系方式
+;(function(){
+    var $modal = $('#ajax-dialog-contact');
+
+    $('#editConcatButton').click(function(event){
+        var remoteUrl = $(this).data('modal-url');
+        $modal.load(remoteUrl, '', function() {
+            $modal.modal({width: 650});
+        });
+    });
+
+    $('#panelContact').on('click','.panel-body',function(){
+    	console.info($(this).data('rowDatas'));
+    	if($(this).hasClass('checked')){
+    		$(this).removeClass('checked');
+    	}else{
+    		$(this).addClass('checked').siblings('.panel-body').removeClass('checked');
+    	}
+    });
+    
+})();
+
+
+
+
